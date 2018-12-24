74366a6a5bcb28fc3c960ba95cb23c22f9783a69
gulve <abhijeet.gulve@tomtom.com>
2018-12-24 16:50:39 +0530

    ctci-> 4.2

---
+++ src/main/java/trees/and/graphs/model/MinimalHeightTree.java
@@ -0,0 +1,28 @@
+package trees.and.graphs.model;
+
+
+/**
+ * @author Abhijeet Gulve
+ */
+public class MinimalHeightTree {
+    public static void main(String[] args) {
+        int[] array =
+                {
+                        1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+                };
+        TreeNode bst = createBST(array, 0, 9);
+        System.out.println(bst);
+
+    }
+
+    public static TreeNode createBST(int[] array, int i, int j) {
+        if (i > j) {
+            return null;
+        }
+        int middle =( i + j) / 2;
+        TreeNode treeNode = new TreeNode(array[middle]);
+        treeNode.left = createBST(array, i, middle - 1);
+        treeNode.right = createBST(array, middle+1, j);
+        return treeNode;
+    }
+}
---
+++ src/main/java/trees/and/graphs/model/TreeNode.java
@@ -0,0 +1,15 @@
+package trees.and.graphs.model;
+
+/**
+ * @author Abhijeet Gulve
+ */
+public class TreeNode {
+
+    TreeNode left;
+    TreeNode right;
+    int data;
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+}
