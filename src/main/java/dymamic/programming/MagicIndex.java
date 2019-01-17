9270715e3e42593d399d9caff90c225cbadf0f30
gulve <abhijeet.gulve@tomtom.com>
2019-01-17 20:14:44 +0530

    coin problem

--- src/main/java/dymamic/programming/MagicIndex.java
+++ src/main/java/dymamic/programming/MagicIndex.java
@@ -7,7 +7,6 @@ public class MagicIndex {
 
     static int binarySearch(int arr[], int low, int high) {
         if (high >= low) {
-            /* low + (high - low)/2; */
             int mid = (low + high) / 2;
             if (mid == arr[mid])
                 return mid;
@@ -16,13 +15,11 @@ public class MagicIndex {
             else
                 return binarySearch(arr, low, (mid - 1));
         }
-
         return -1;
     }
 
-    //main function
     public static void main(String args[]) {
-        int arr[] = {-10, -1, 0, 3, 10, 11, 30, 50, 100};
+        int[] arr = {-10, -1, 0, 3, 10, 11, 30, 50, 100};
         int n = arr.length;
         System.out.println("Fixed Point is "
                 + binarySearch(arr, 0, n - 1));
